811c3e8a2d4c17cbf1c9b9907e650564fd1207f8
Mohammad MDSA <samimd.77@gmail.com>
2017-11-12 00:13:26 +0330

    Add user message handler

---
+++ MongoInterface.js
@@ -0,0 +1,74 @@
+require('dotenv-extended').load();
+const MongoClient = require('mongodb').MongoClient;
+const Address = process.env.MONGODB_ADDRESS;
+
+module.exports = class DB {
+	async insert(collName, object, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			db.collection(collName).insertOne(object, (err, result) => {
+				if (err) throw err;
+				db.close();
+				callback();
+			});
+		});
+	}
+	async insertMany(collName, object, callback) {
+		MongoClient.connect(Address, (err, db) => {
+			db.collection(collName).insertMany(object, (err, res) => {
+				if (err) throw err;
+				db.close();
+				callback();
+			});
+		});
+	}
+	async findOne(collName, condition, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			if (err) throw err;
+			db.collection(collName).findOne(condition, (err, result) => {
+				if (err) throw err;
+				db.close();
+				callback(result);
+			});
+		});
+	}
+	async find(collName, condition, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			if (err) throw err;
+			db.collection(collName).find(condition).toArray((err, result) => {
+				if (err) throw err;
+				db.close();
+				callback(result);
+			});
+		});
+	}
+	async updateOneDocument(collName, condition, updatedDocument, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			if (err) throw err;
+			db.collection(collName).updateOne(condition, updatedDocument, (err, res) => {
+				if (err) throw err;
+				db.close();
+				callback();
+			});
+		});
+	}
+	async updateOneField(collName, condition, updatedField, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			if (err) throw err;
+			db.collection(collName).updateOne(condition, { $set: updatedField }, (err, res) => {
+				if (err) throw err;
+				db.close();
+				callback();
+			});
+		});
+	}
+	async updateFields(collName, condition, updatedField, callback = () => { }) {
+		await MongoClient.connect(Address, (err, db) => {
+			if (err) throw err;
+			db.collection(collName).updateMany(condition, { $set: updatedField }, (err, res) => {
+				if (err) throw err;
+				db.close();
+				callback();
+			});
+		});
+	}
+};
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import { connect } from 'http2';
-
 require('dotenv-extended').load();
 
 const builder = require('botbuilder');
 const restify = require('restify');
 const { Prompts, UniversalBot, ChatConnector, Message } = builder;
+const Mongo = require('./MongoInterface');
+
+let db = new Mongo();
 
 const server = restify.createServer();
 server.listen(process.env.PORT, () => {
@@ -13,10 +14,53 @@ server.listen(process.env.PORT, () => {
 
 let connector  = new ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
-	appPassword: MICROSOFT_APP_PASSWORD
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 server.post('/api/messages', connector.listen());
 
-let bot = new UniversalBot(connector, [
-	// bot code goes here...
+let bot = new UniversalBot(connector, (session) => {
+	session.replaceDialog('root', {isFirst: true});
+});
+
+bot.dialog('root', [
+	(session, args) => {
+		let msg = new Message(session)
+			.textFormat('plain');
+		if(args && args.isFirst)
+			msg.text('سلام...\nپیام بده');
+		else
+			msg.text('پیام بده');
+		// session.send({args});
+		session.sendTyping();
+		Prompts.text(session, msg);
+	},
+	(session, results) => {
+		session.sendTyping();
+		db.insert('pendingMessages', {message: results}, () => {
+			session.send('ببینم جی میشه...');
+			Prompts.choice(session, 'باز میخوای بفرستی؟', ['آره!'], {listStyle: 3});
+		});
+	},
+	(session, results) => {
+		if(results.response.entity === 'آره!')
+			session.replaceDialog('root', {isFirst: false})
+		else
+			session.replaceDialog('requester');
+	}
+]);
+
+bot.dialog('requester', [
+	(session) => {
+		let msg = new Message(session)
+			.textFormat('plain')
+			.text('والا نفهمیدم چی میگی...');
+		session.send(msg);
+		Prompts.choice(session, 'اگه خواستی بفرستی بگو!!!!!', ['میخوام!'], {listStyle: 3});
+	},
+	(session, results) => {
+		if(results.response.entity === 'میخوام!')
+			session.replaceDialog('root', {isFirst: false});
+		else
+			session.replaceDialog('requester');
+	}
 ]);
