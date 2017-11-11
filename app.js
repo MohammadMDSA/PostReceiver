37cd5c75a18a0f66a078d37817b25990c04d8ac7
Mohammad MDSA <samimd.77@gmail.com>
2017-11-12 01:33:12 +0330

    Add custom prompt to handle to recieve every kind of response

--- app.js
+++ app.js
@@ -12,55 +12,50 @@ server.listen(process.env.PORT, () => {
 	console.log(`${server.name} listening to ${server.url}`);
 });
 
-let connector  = new ChatConnector({
+let connector = new ChatConnector({
 	appId: process.env.MICROSOFT_APP_ID,
 	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 server.post('/api/messages', connector.listen());
 
 let bot = new UniversalBot(connector, (session) => {
-	session.replaceDialog('root', {isFirst: true});
+	session.replaceDialog('root', { isFirst: true });
 });
 
 bot.dialog('root', [
 	(session, args) => {
 		let msg = new Message(session)
 			.textFormat('plain');
-		if(args && args.isFirst)
+		if (args && args.isFirst)
 			msg.text('سلام...\nپیام بده');
 		else
 			msg.text('پیام بده');
 		// session.send({args});
 		session.sendTyping();
-		Prompts.text(session, msg);
-	},
-	(session, results) => {
+		// Prompts.attachment(session, msg);
+		Prompts.PostPrompt(session, msg);
+	},(session, results) => {
 		session.sendTyping();
-		db.insert('pendingMessages', {message: results}, () => {
+		db.insert('pendingMessages', { message: results }, () => {
 			session.send('ببینم جی میشه...');
-			Prompts.choice(session, 'باز میخوای بفرستی؟', ['آره!'], {listStyle: 3});
+			Prompts.choice(session, 'باز میخوای بفرستی؟', ['آره!', 'نه!'], { listStyle: 3, retryPrompt: 'والا نفهمیدم چی میگی... میخوای باز پست بدی؟' });
 		});
 	},
 	(session, results) => {
-		if(results.response.entity === 'آره!')
-			session.replaceDialog('root', {isFirst: false})
+		if (results.response.entity === 'آره!')
+			session.replaceDialog('root', { isFirst: false })
 		else
-			session.replaceDialog('requester');
+			session.endConversation('پس به سلامت...');
 	}
 ]);
 
-bot.dialog('requester', [
-	(session) => {
-		let msg = new Message(session)
-			.textFormat('plain')
-			.text('والا نفهمیدم چی میگی...');
-		session.send(msg);
-		Prompts.choice(session, 'اگه خواستی بفرستی بگو!!!!!', ['میخوام!'], {listStyle: 3});
-	},
-	(session, results) => {
-		if(results.response.entity === 'میخوام!')
-			session.replaceDialog('root', {isFirst: false});
-		else
-			session.replaceDialog('requester');
-	}
-]);
+let _postPrompt = new builder.Prompt({ defaultRetryPrompt: 'والا نفهمیدم چی میگی... میخوای باز پست بدی؟' })
+	.onRecognize((context, callback) => {
+		callback(null, 1.0, context.message);
+	});
+bot.dialog('PostPrompt', _postPrompt);
+builder.Prompts.PostPrompt = (session, prompt, options) => {
+	let args = options || {};
+	args.prompt = prompt || options.prompt;
+	session.beginDialog('PostPrompt', args);
+}
